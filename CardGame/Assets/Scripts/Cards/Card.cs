fc46d3385e1912e832ac32a217a729c0a4843a0f
kafkaphoenix <jaguilerapuerta@gmail.com>
2018-11-30 12:34:02 +0100

    Added Card and CardViz Scripts
    Cards now are templates which can be randomly created on start (there is a pool of stats, levels, names, photos)

---
+++ CardGame/Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cards
+{
+    [CreateAssetMenu(menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string title;
+        public string level;
+        public Sprite art;
+        public string cardDetail;
+        public string secondaryText;
+        public string treasures;
+        public Sprite background;
+        public Sprite template;
+
+    }
+}
+
---
+++ CardGame/Assets/Scripts/Cards/CardViz.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Cards
+{
+    public class CardViz : MonoBehaviour
+    {
+        public Text title;
+        public Text level;
+        public Text cardDetail;
+        public Text secondaryText;
+        public Text treasures;
+        public Image art;
+        public Image background;
+        public Image template;
+
+        public Card card;
+
+        private void Start()
+        {
+            LoadCard(card);
+        }
+
+        public void LoadCard(Card c)
+        {
+            if (c == null)
+                return;
+
+            card = c;
+            title.text = c.title;
+            art.sprite = c.art;
+            cardDetail.text = c.cardDetail;
+            background.sprite = c.background;
+            template.sprite = c.template;
+
+            if (string.IsNullOrEmpty(c.level))
+            {
+                level.gameObject.SetActive(false);
+            }
+            else
+            {
+                level.gameObject.SetActive(true);
+                level.text = c.level;
+            }
+            if (string.IsNullOrEmpty(c.secondaryText))
+            {
+                secondaryText.gameObject.SetActive(false);
+            } else
+            {
+                secondaryText.gameObject.SetActive(true);
+                secondaryText.text = c.secondaryText;
+            }
+            if (string.IsNullOrEmpty(c.treasures))
+            {
+                treasures.gameObject.SetActive(false);
+            }
+            else
+            {
+                treasures.gameObject.SetActive(true);
+                treasures.text = c.treasures;
+            }
+        }
+    }
+}
+
